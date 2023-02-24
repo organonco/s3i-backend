aefc7d82e65ac218213edf7f2a60ae6ee5587851
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2023-02-24 23:40:45 +0300

    fix dashboard

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -39,6 +39,9 @@ class HandleInertiaRequests extends Middleware
                     'location' => $request->url(),
                 ]);
             },
+            'metadata' => [
+                'route' => $request->route()->uri
+            ]
         ]);
     }
 }
--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,3 +1,21 @@
+
+<script setup>
+import { Link } from '@inertiajs/vue3';
+</script>
+<script>
+export default {
+    data() {
+        let currentRoute = this.$page.props.metadata.route;
+        return {
+            listItems: [
+                { icon: "mdi-home-city", title: "Home", 'route': 'dashboard', 'value': 'dashboard' },
+            ]
+        }
+    }
+}
+</script>
+
+
 <template>
     <v-card>
         <v-layout>
@@ -8,26 +26,23 @@
                 <template v-slot:prepend>
                     <v-list-item
                         lines="two"
-                        prepend-avatar="https://randomuser.me/api/portraits/women/81.jpg"
-                        title="Jane Smith"
-                        subtitle="Logged in"
-                    ></v-list-item>
+                        prepend-avatar="https://www.citypng.com/public/uploads/small/11640168385jtmh7kpmvna5ddyynoxsjy5leb1nmpvqooaavkrjmt9zs7vtvuqi4lcwofkzsaejalxn7ggpim4hkg0wbwtzsrp1ldijzbdbsj5z.png"
+                        :title="$page.props.auth.user.name"
+                    >
+                        <Link :href="route('logout')" method="post" class="underline">
+                            Log Out
+                        </Link>
+                    </v-list-item>
                 </template>
                 <v-divider></v-divider>
                 <v-list density="compact" nav>
-                    <v-list-item prepend-icon="mdi-home-city" title="Home" value="home"></v-list-item>
-                    <v-list-item prepend-icon="mdi-account" title="My Account" value="account"></v-list-item>
-                    <v-list-item prepend-icon="mdi-account-group-outline" title="Users" value="users"></v-list-item>
+                    <v-list-item v-for="listItem in listItems" :prepend-icon="listItem.icon" :title="listItem.title" :value="listItem.value" :active="listItem.value == $page.props.metadata.route"></v-list-item>
                 </v-list>
             </v-navigation-drawer>
-            <v-main style="height: 250px"></v-main>
+            <v-main>
+                <slot></slot>
+            </v-main>
         </v-layout>
     </v-card>
 
 </template>
-
-<script>
-export default {
-    //
-}
-</script>
