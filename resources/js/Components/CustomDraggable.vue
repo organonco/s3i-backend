9ea4c09ae14c056f0643bc8a7fd2fd662aca50c9
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2023-03-22 19:55:54 +0300

    add custom draggable component

---
+++ resources/js/Components/CustomDraggable.vue
@@ -0,0 +1,33 @@
+<script setup>
+import draggable from "vuedraggable";
+</script>
+
+<script>
+export default {
+    components: [draggable],
+    props: ['modelValue'],
+    emits: ['update:modelValue'],
+    computed: {
+        value: {
+            get() {return this.modelValue},
+            set(value) {this.$emit('update:modelValue', value)}
+        },
+        dragOptions() {
+                return {
+                    animation: 250,
+                    group: "people",
+                    disabled: false,
+                    ghostClass: "ghost"
+                };
+            },
+    }
+}
+</script>
+
+<template>
+    <draggable v-model="value" item-key="id" v-bind="dragOptions">
+        <template #item="{element}">
+            <div> {{element.name}} </div>
+        </template>
+    </draggable>
+</template>
