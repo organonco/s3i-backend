f5b18f0dff0bfe62ffe3cc7b3a63c6674cfc3179
Mhd Ghaith Alhelwany <ghaith.helwani@eprocurement.ae>
2023-05-16 17:18:17 +0300

    fix show quiz API

--- app/Http/Controllers/API/Course/QuizController.php
+++ app/Http/Controllers/API/Course/QuizController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\API\Course;
 
+use App\Exceptions\NotFound;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\API\Course\Quiz\ShowQuizRequest;
 use App\Http\Resources\API\Course\Items\Quiz\CourseQuizAPIResource;
+use App\Models\Course;
+use App\Models\CourseItem;
 use App\Models\CourseQuiz;
 
 /**
@@ -16,11 +19,18 @@ class QuizController extends Controller
 
     /**
      * Show
+     * @throws NotFound
+     * @authenticated
      * @responseFile app/Http/Responses/Samples/Quiz/show.json
+     * @responseFile 404 app/Http/Responses/Samples/not-found.json
      */
-    public function show(CourseQuiz $quiz, ShowQuizRequest $request)
+    public function show(string $item_id, ShowQuizRequest $request)
     {
-        return new CourseQuizAPIResource($quiz);
+        $item = CourseItem::byHash($item_id);
+        if($request->user()->can('viewDetails', [Course::class, $item->course_id]) && $item->item instanceof CourseQuiz)
+            return CourseQuizAPIResource::make($item->item);
+        else
+            throw new NotFound;
     }
 
 }
