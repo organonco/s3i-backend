c502b561219b71328a8e638e1167e38372a416eb
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2023-06-23 22:58:58 +0300

    fix theme

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,7 @@
-import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import {createApp, h} from 'vue';
+import {createInertiaApp} from '@inertiajs/vue3';
+import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers';
+import {ZiggyVue} from '../../vendor/tightenco/ziggy/dist/vue.m';
 import {createVuetify} from "vuetify";
 
 import * as components from 'vuetify/components'
@@ -9,25 +9,44 @@ import * as directives from 'vuetify/directives'
 import 'vuetify/styles'
 
 import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
+import {VDataTable} from 'vuetify/labs/VDataTable'
 
-import { VDataTable } from 'vuetify/labs/VDataTable'
+const myCustomLightTheme = {
+    dark: false,
+    options: {
+        customProperties: true
+    },
+    colors: {
+        surface: '#F5F5F5',
+        primary: '#6200EE',
+        secondary: '#03DAC6',
+        error: '#B00020',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FB8C00',
+    },
+}
 
 
 const vuetify = createVuetify({
     components: {...components, VDataTable},
     directives,
-    icons: {
-        defaultSet: 'mdi', // This is already the default value - only for display purposes
+    theme: {
+        defaultTheme: 'myCustomLightTheme',
+        themes: {
+            myCustomLightTheme,
+        },
     },
 })
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
+
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
-    setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
+    setup({el, App, props, plugin}) {
+        return createApp({render: () => h(App, props)})
             .use(plugin)
             .use(ZiggyVue, Ziggy)
             .use(vuetify)
