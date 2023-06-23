96f0c3ff8e1e9e87983aa2a5456e1215370d0ced
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2023-06-23 22:58:52 +0300

    fix theme

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,7 +15,7 @@
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
-    <body class="font-sans antialiased">
+    <body class="font-sans antialiased" style="background-color: #FAFAFA">
         @inertia
     </body>
 </html>
