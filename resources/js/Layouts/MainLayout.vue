aa4ec868e20188d47638bac5486c447655e05489
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2023-09-02 01:23:33 +0300

    update build

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -3,7 +3,7 @@ import {Head, Link} from '@inertiajs/vue3';
 import {adminMenuItems, teacherMenuItems} from "@/Menus/NavMenu";
 </script>
 <script>
-const version = "1.1.1";
+const version = "1.1.2";
 
 export default {
     props: {
