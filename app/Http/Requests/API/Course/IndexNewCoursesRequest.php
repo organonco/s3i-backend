a9d202dc2d0c2bd0e1ee8a1166b498523e8fed40
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2023-09-12 22:52:07 +0300

    fix new courses API

--- app/Http/Requests/API/Course/IndexNewCoursesRequest.php
+++ app/Http/Requests/API/Course/IndexNewCoursesRequest.php
@@ -4,9 +4,6 @@ namespace App\Http\Requests\API\Course;
 
 use App\Http\Requests\API\Request;
 
-/**
- * @queryParam category_id optional
- */
 class IndexNewCoursesRequest extends Request
 {
     public function authorize(): bool
