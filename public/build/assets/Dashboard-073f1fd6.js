913fa2df8e9fed5d7b5e5ac0290a3ec13f23bfc6
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2023-09-20 02:19:16 +0300

    build

---
+++ public/build/assets/Dashboard-073f1fd6.js
@@ -0,0 +1,1 @@
+import{_ as a}from"./MainLayout-f8f94285.js";import{o as t,f as e,w as o,b as r}from"./app-2855b29f.js";const m={__name:"Dashboard",setup(s){return(_,c)=>(t(),e(a,{title:"Dashboard"},{default:o(()=>[r(" Hi ")]),_:1}))}};export{m as default};
