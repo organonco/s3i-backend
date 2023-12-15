433d2bf95d773e0cd3bbd06fcfb5a57dcdd53c2a
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2023-12-15 17:34:14 +0300

    add front-end build

---
+++ public/build/assets/Dashboard-37908514.js
@@ -0,0 +1,1 @@
+import{_ as a}from"./MainLayout-c61a0cbd.js";import{o as t,f as e,w as o,b as r}from"./app-ad12d3a9.js";const m={__name:"Dashboard",setup(s){return(_,c)=>(t(),e(a,{title:"Dashboard"},{default:o(()=>[r(" Hi ")]),_:1}))}};export{m as default};
