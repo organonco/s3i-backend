e4921d262685a13c8c24eb1ff3f102d248fa64a3
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2023-12-18 20:03:54 +0300

    change version

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -3,7 +3,7 @@ import {Head, Link} from '@inertiajs/vue3';
 import {adminMenuItems, teacherMenuItems} from "@/Menus/NavMenu";
 </script>
 <script>
-const version = "1.2.0";
+const version = "1.2.2";
 
 export default {
     props: {
